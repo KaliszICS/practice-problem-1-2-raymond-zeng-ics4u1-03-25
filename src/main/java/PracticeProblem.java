bc0ed226ee628ad0930b9598c93b60a48917abc7
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-03-29 19:48:07 +0000

    update files

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,32 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static boolean validIndex(int [] arr, int num){
+		 try{
+			int x = arr[num];
+				return true;
+			}
+		 catch (ArrayIndexOutOfBoundsException e){
+			return false;
+		 }
+	}
+	public static int divide(int num1, int num2){
+		try {
+			int x = num1/num2;
+			return x;
+		}
+		catch (ArithmeticException e){
+			return 0;
+		}
+	}
+	public static int safeConvertStringtoInt(String num){
+		try {
+			int number = Integer.parseInt(num);
+			return number;
+		}
+		catch (NumberFormatException e){
+			return 0;
+		}
+	}
 
 }
